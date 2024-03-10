3066faa734ea870ab19fbfe3c3550b126eef8c8f
Rajdeep05-web <121rajpaul@gmail.com>
2024-03-10 15:55:15 +0530

    axios instance created and request intercept added to send req header to auth middleware

--- Client/src/api/index.js
+++ Client/src/api/index.js
@@ -1,20 +1,35 @@
 import axios from 'axios';
 
-// const url = "http://localhost:3001/post";
+
+
+const API = axios.create({ baseURL: 'http://localhost:3001' }); //creating an instance of axios to make requests to the server
+
+
+//interceptors - Interceptors are methods which are triggered before or after the main method is executed. (Types : request and response interceptors)
+API.interceptors.request.use((req) => {
+
+    if(localStorage.getItem('profile')){//if the user is logged in
+
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;//we set the token in the headers
+
+    }
+
+    return req;
+})
 
 //post APIs
-export const fetchPosts = () => axios.get("http://localhost:3001/get");//fetching posts from the api
+export const fetchPosts = () => API.get("/get");//fetching posts from the api
 
-export const createPosts = (newPost) => axios.post("http://localhost:3001/post", newPost);//Creating posts from the api
+export const createPosts = (newPost) => API.post("/post", newPost);//Creating posts from the api
 
-export const updatePosts = (id, post) => axios.patch(`http://localhost:3001/update/${id}`, post);//Updating posts from the api
+export const updatePosts = (id, post) => API.patch(`/update/${id}`, post);//Updating posts from the api
 
-export const deletePosts = (id) => axios.delete(`http://localhost:3001/delete/${id}`);//Deleting posts from the api
+export const deletePosts = (id) => API.delete(`/delete/${id}`);//Deleting posts from the api
 
-export const likePosts = (id) => axios.patch(`http://localhost:3001/likePost/${id}`);//Liking posts from the api
+export const likePosts = (id) => API.patch(`/likePost/${id}`);//Liking posts from the api
 
 
 //user auth APIs
-export const signInAPI = (formData) => axios.post("http://localhost:3001/signin", formData)//Signing in from the api
+export const signInAPI = (formData) => API.post("/signin", formData)//Signing in from the api
 
-export const signUpAPI = (formData) => axios.post("http://localhost:3001/signup", formData)//Signing up from the api
+export const signUpAPI = (formData) => API.post("/signup", formData)//Signing up from the 
