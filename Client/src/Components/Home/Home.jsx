765eecf380efadb96674f654d773f31519f4bad0
Rajdeep05-web <121rajpaul@gmail.com>
2024-03-11 17:25:16 +0530

    Some bug fixed

--- Client/src/Components/Home/Home.jsx
+++ Client/src/Components/Home/Home.jsx
@@ -1,16 +1,11 @@
-
-
 import React, {useEffect, useState} from "react";
 import {useDispatch} from "react-redux";
-
-
-
 
 //components
 import Posts from "../Posts/Posts";
 import Form from "../Forms/Form";
 
-
+//style
 import './style.css'
 
 //redux actions
@@ -20,27 +15,30 @@ const Home = () => {
 
     const [updatePost, setUpdatePost] = useState(null);//state to store the post to be updated
  
-
     const dispatch = useDispatch();
     
-  
     //importent
     useEffect(() => {//useEffect to dispatch action when the component mounts
       dispatch(getPosts());//it dispatches the getPosts action to get the posts from the server and updates the state of the redux store. So then the useSelector hook in the Posts component can get the posts from the state store of redux.
     });//no dependency array means it will run only once when the component mounts(rendered for the first time) and on every re-render
   
   
-
-
     return(
         <section className="app-body">
-        <section className="posts">
-          <Posts setUpdatePost={setUpdatePost} />
+
+          <section className="posts">
+
+            <Posts setUpdatePost={setUpdatePost} />
+
+          </section>
+
+          <section className="form">
+
+            <Form updatePost={updatePost} setUpdatePost={setUpdatePost}/>
+
+          </section>
+
         </section>
-        <section className="form">
-          <Form updatePost={updatePost} setUpdatePost={setUpdatePost}/>
-        </section>
-    </section>
     )
 }
 
--- Client/src/Components/Navbar/Navbar.jsx
+++ Client/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import {useNavigate, useLocation} from 'react-router-dom';
 import {useDispatch} from 'react-redux';
+import {jwtDecode} from 'jwt-decode';
 
 //images
 import memories from "../../images/memories.png";
@@ -18,22 +19,37 @@ const [user, setUser] = useState(null);//getting the user from the local storage
  const location = useLocation();//useLocation -> returns the location object that represents the current URL. You can think about it like a useState that returns a new location whenever the URL changes.
  const dispatch = useDispatch();//useDispatch is a hook from react-redux.
 
- useEffect(() => {
-
-     setUser(JSON.parse(localStorage.getItem('profile')))//getting the user from the local storage
-
-    },[location])//whenever the location changes the useEffect will run
-    // useEffect(() => {
-    //   dispatch(getPosts());
-    // })
+ //log in
  const Auth = () => {
    
    navigate('/auth')//this will take you to the auth page
   }
+  //log out the user
   const handlelogout = () => {
     dispatch({type: 'LOGOUT'});//dispatching an action with type LOGOUT
     setUser(null);//setting the user to null
+    window.location.reload();
   }
+  //getting the user from the local storage when the url changes auth to home page after signpu/login
+  useEffect(() => {
+
+     setUser(JSON.parse(localStorage.getItem('profile')))//getting the user from the local storage
+
+  },[location])//whenever the location changes the useEffect will run
+    
+    //token expiration
+    useEffect(()=>{
+      const token = user?.token;//getting the token from the user
+      if(token){
+        const decodedToken = jwtDecode(token);//decoding the token
+        // console.log(decodedToken.exp * 1000);
+        // console.log(new Date().getTime());
+         if(decodedToken.exp*1000 < new Date().getTime()){
+            handlelogout();//if the token is expired then logout the user
+         }
+      }
+    })
+
 
     return (
         <section className="app-bar">
