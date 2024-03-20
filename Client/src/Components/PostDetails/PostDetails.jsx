8f272f25a87d19453018d79368b48970078ab5cf
Rajdeep05-web <121rajpaul@gmail.com>
2024-03-21 00:57:40 +0530

    Post details css added

--- Client/src/Components/PostDetails/PostDetails.jsx
+++ Client/src/Components/PostDetails/PostDetails.jsx
@@ -1,39 +1,52 @@
-import React, {useEffect} from 'react';
+import React from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import {getPost} from '../../actions/posts';
 import { useParams, useNavigate } from 'react-router-dom';
 
 import './style.css';
 
-import {getPost} from '../../actions/posts';
 
 const PostDetails = () => {
-
-    const {post} = useSelector((state) => state.posts );
-    const {id} = useParams(); 
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    const {id} = useParams(); 
+    
     useEffect(() => {
         dispatch(getPost(id));
-    },[id]);   
+    },[id,dispatch]);
+        
+    const {post} = useSelector((state) => state.posts );
     
-    if(!post) return null;
-
-    return (
+    
+    if(!post){
+        return (
+            <div>
+                {/* <h1>Post not found</h1> */}
+                <div class="loader4"></div>
+            </div>
+        );
+    }
+    
+    return(
         <div className='post-container'>
-          <section className='details'>
-                <h1>{post.title}</h1>
-                <p>{post.message}</p>
-                <p>{post.tags.map((tag) => `#${tag} `)}</p>
-                <p>{post.name}</p>
-                <p>{post.likes.length} likes</p>
-          </section>
-          <img src={post.selectedFile} alt="img" className='post-img'/>
-            
-            
-        </div>
+        <section className='details'>
+              <p className='creator' >Creator : {post?.name}</p>
+              <h1 className='place' >Place : {post?.title}</h1>
+              <p className='message'> Message : {post?.message}</p>
+              <p className='tags' > Tags : {post?.tags.map((tag) => `#${tag} `)}</p>
+              <p className='like' >{post?.likes.length} {post?.likes.length>1 ? "Likes" : "Like"}</p>
+        </section>
+        <img src={post?.selectedFile} alt="img" className='post-img'/>
+          
+          
+      </div>
     )
+
+    
+    
 }
 
 export default PostDetails;
